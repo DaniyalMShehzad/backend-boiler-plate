c15af34d1ed07cb9283ffe21b32b81f093558720
DaniyalMShehzad <daniyalshehzad305@gmail.com>
2023-06-18 17:20:09 +0500

    first commit

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const port = process.env.PORT || 8000;
 const app = require("./app");
 const cors = require("cors");
 const server = http.createServer(app);
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  `mongodb+srv://daniyalshehzad41:c4SwLPVpd9zdbz5i@cluster0.o6zxlmh.mongodb.net/test`
+);
+mongoose.connection.on("error", (err) => {
+  console.log("connection failed", err);
+});
+mongoose.connection.on("connected", (err) => {
+  console.log("connected successfully With database ");
+});
 app.use(cors());
 server.listen(port, () => {
   console.log(`apps are running on localhost: "+${port}`);
